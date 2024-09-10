8ac917b988af51070a10e01e27962fd625328e04
Hasan İncel <h_incel@hotmail.com>
2024-09-11 02:03:50 +0300

    Add project files.

---
+++ Basic_MVC/Controllers/CustomerOrdersController.cs
@@ -0,0 +1,34 @@
+﻿using Basic_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Basic_MVC.Controllers
+{
+    public class CustomerOrdersController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Örnek müşteri ve siparişler oluşturma
+            var customer = new Customer
+            {
+                Id = 1,
+                FirstName = "Ajda",
+                LastName = "Pekkan",
+                Email = "ajda.pekkan@gmail.com"
+            };
+
+            var orders = new List<Order>
+            {
+                new Order { Id = 1, ProductName = "Laptop", Price = 3000.00m, Quantity = 1 },
+                new Order { Id = 2, ProductName = "Mouse", Price = 150.00m, Quantity = 2 }
+            };
+
+            var viewModel = new CustomerOrderViewModel
+            {
+                Customer = customer,
+                Orders = orders
+            };
+
+            return View(viewModel);
+        }
+    }
+}
